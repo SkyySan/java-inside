bf038dbfc8060c4fc6d8f567764886b34990016f
Simon Assié <simassie@orange.fr>
2020-10-06 08:36:07 +0200

    lab3

--- lab2/src/test/java/fr/umlv/javainside/JSONPrinterTest.java
+++ lab2/src/test/java/fr/umlv/javainside/JSONPrinterTest.java
@@ -2,12 +2,19 @@ package fr.umlv.javainside;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.RecordComponent;
 
 import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JSONPrinterTest {
+
+
 
     private record Alien(int age, String planet) {
         public Alien {
@@ -18,7 +25,7 @@ public class JSONPrinterTest {
         }
     }
 
-    private record Person(String firstName, String lastName) {
+    private record Person(@JSONPrinter.JSONProperty("first-name") String firstName, @JSONPrinter.JSONProperty("last-name")String lastName) {
         public Person {
             requireNonNull(firstName);
             requireNonNull(lastName);
@@ -28,7 +35,7 @@ public class JSONPrinterTest {
     @Test
     public void toJSON(){
         var person = new Person("John", "Doe");
-        assertEquals(IncompleteJSONParser.parse("{ \"firstName\": \"John\", \"lastName\": \"Doe\"}"),IncompleteJSONParser.parse(JSONPrinter.toJSON(person)));
+        assertEquals(IncompleteJSONParser.parse("{ \"first-name\": \"John\", \"last-name\": \"Doe\"}"),IncompleteJSONParser.parse(JSONPrinter.toJSON(person)));
         var alien = new Alien(100, "Saturn");
         assertEquals(IncompleteJSONParser.parse("{ \"age\": 100, \"planet\": \"Saturn\"}"),IncompleteJSONParser.parse(JSONPrinter.toJSON(alien)));
     }
