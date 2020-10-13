93d69e62c24da63f7de93ee1f54899888d9f05e2
Simon Assié <simassie@orange.fr>
2020-10-13 17:18:34 +0200

    Q8

--- lab4/src/main/java/fr/umlv/javainside/Logger.java
+++ lab4/src/main/java/fr/umlv/javainside/Logger.java
@@ -2,7 +2,10 @@ package fr.umlv.javainside;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.invoke.MutableCallSite;
 import java.lang.reflect.UndeclaredThrowableException;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 import static java.lang.invoke.MethodType.methodType;
@@ -10,7 +13,9 @@ import static java.lang.invoke.MethodType.methodType;
 public interface Logger {
     public void log(String message);
 
-    public static Logger of(Class<?> declaringClass, Consumer<? super String> consumer) {
+    public static Logger of(Class<?> declaringClass, Consumer<? super String> consumer) throws NoSuchMethodException, IllegalAccessException {
+        Objects.requireNonNull(declaringClass);
+        Objects.requireNonNull(consumer);
         var mh = createLoggingMethodHandle(declaringClass, consumer);
         return new Logger() {
             @Override
@@ -26,8 +31,65 @@ public interface Logger {
         };
     }
 
-    private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer) {
-        
-        return null;
+    public static Logger lambdaOf(Class<?> declaringClass, Consumer<? super String> consumer) throws NoSuchMethodException, IllegalAccessException {
+        Objects.requireNonNull(declaringClass);
+        Objects.requireNonNull(consumer);
+        var mh = createLoggingMethodHandle(declaringClass, consumer);
+        return (message) -> {
+            try {
+                mh.invokeExact(message);
+            } catch(RuntimeException | Error e) {
+                throw e;
+            } catch(Throwable t) {
+                throw new UndeclaredThrowableException(t);
+            }
+        };
+    }
+
+    public static Logger recordOf(Class<?> declaringClass, Consumer<? super String> consumer) throws NoSuchMethodException, IllegalAccessException {
+        Objects.requireNonNull(declaringClass);
+        Objects.requireNonNull(consumer);
+        var mh = createLoggingMethodHandle(declaringClass, consumer);
+        return (message) -> {
+            try {
+                mh.invokeExact(message);
+            } catch(RuntimeException | Error e) {
+                throw e;
+            } catch(Throwable t) {
+                throw new UndeclaredThrowableException(t);
+            }
+        };
+    }
+
+    class Impl{
+        private static final MethodHandle ACCEPT;
+        static {
+            var lookup = MethodHandles.lookup();
+            try {
+                ACCEPT = lookup.findVirtual(Consumer.class,"accept", methodType(void.class, Object.class));
+            } catch (NoSuchMethodException | IllegalAccessException e) {
+                throw new AssertionError(e);
+            }
+        }
+
+        private static final ClassValue<MutableCallSite> ENABLE_CALLSITES = new ClassValue<>() {
+            protected MutableCallSite computeValue(Class<?> type) {
+                return new MutableCallSite(MethodHandles.constant(boolean.class, true));
+            }
+        };
+
+        public static void enable(Class<?> declaringClass, boolean enable) {
+            ENABLE_CALLSITES.get(declaringClass).setTarget(MethodHandles.constant(boolean.class, enable));
+        }
+    }
+
+    private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer) throws NoSuchMethodException, IllegalAccessException {
+        var target = Impl.ACCEPT.bindTo(consumer);
+        target = target.asType(methodType(void.class, String.class));
+
+        var test = Impl.ENABLE_CALLSITES.get(declaringClass).dynamicInvoker();
+        var fallback = MethodHandles.empty(methodType(void.class, String.class));
+
+        return MethodHandles.guardWithTest(test, target, fallback);
     }
 }
--- lab4/src/test/java/fr/umlv/javainside/LoggerTest.java
+++ lab4/src/test/java/fr/umlv/javainside/LoggerTest.java
@@ -1,4 +1,5 @@
 package fr.umlv.javainside;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,14 +7,41 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LoggerTest {
 
     @Test
-    public void ofTest(){
+    public void ofTest() throws NoSuchMethodException, IllegalAccessException {
         class Foo{}
-        var logger = Logger.of(Foo.class, String::toUpperCase);
+        var logger = Logger.of(Foo.class, __ -> {});
         assertNotNull(logger);
     }
 
     @Test
-    public void logTest(){
+    public void ofError(){
+        class Foo{}
+        assertAll(
+                ()-> assertThrows(NullPointerException.class,()-> Logger.of(null, __-> {})),
+                ()-> assertThrows(NullPointerException.class,()-> Logger.of(Foo.class, null))
+        );
+    }
 
+    @Test
+    public void logTest() throws NoSuchMethodException, IllegalAccessException {
+        class Foo{}
+        var logger = Logger.of(Foo.class, message -> {
+            assertEquals("hello", message);
+        });
+        logger.log("hello");
+    }
+
+    @Test
+    public void logWithNullAsValue() throws NoSuchMethodException, IllegalAccessException {
+        class Foo{}
+        var logger = Logger.of(Foo.class, Assertions::assertNull);
+        logger.log(null);
+    }
+
+    @Test
+    public void lambdaOfTest() throws NoSuchMethodException, IllegalAccessException {
+        class Foo{}
+        var logger = Logger.lambdaOf(Foo.class, __ -> {});
+        assertNotNull(logger);
     }
 }
